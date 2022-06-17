40d1581ad0b019addfbf3f4124631f549e89456c
thiagogcorreabotelho2021@outlook.com <99252640+thiagocorreabotelho@users.noreply.github.com>
2022-06-17 17:34:05 -0300

    Subindo versão da api estável

---
+++ APITabelaFip/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using APITabelaFip;
+using System.Text.Json;
+
+Console.WriteLine("Digite o modelo do seu veículo! (Carros) (Motos) (Caminhões)");
+
+var tipoVeiculos = Console.ReadLine();
+
+if (string.IsNullOrEmpty(tipoVeiculos))
+{
+    Console.WriteLine("Infelizmente não podemos dar continuidade na consulta, pois o tipo fornecedido é inválido! \n Por favor tente novamente ou mais tarde.");
+}
+
+var endpoint = $"https://parallelum.com.br/fipe/api/v1/{tipoVeiculos}/marcas";
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage? response = await client.GetAsync(endpoint);
+    response.EnsureSuccessStatusCode();
+
+    string retornoBody = await response.Content.ReadAsStringAsync();
+
+    List<Veiculo> veiculos = JsonSerializer.Deserialize<List<Veiculo>>(retornoBody);
+
+    foreach (var item in veiculos)
+    {
+        Console.WriteLine($"Marca do {tipoVeiculos} é: {item.nome} - O código do {tipoVeiculos} é {item.codigo}");
+    }
+}
+catch (Exception)
+{
+
+    throw;
+}
---
+++ APITabelaFip/Veiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITabelaFip
+{
+    public class Veiculo
+    {
+        public string codigo { get; set; }  
+        public string nome { get; set; }
+    }
+}
